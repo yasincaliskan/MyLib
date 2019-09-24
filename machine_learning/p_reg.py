4d36097d12a6c595b4ad71bb529e4a4e5c2f3d16
Yasin Çalışkan <yaskocaliskan26@gmail.com>
2019-09-24 19:56:09 +0300

    polynomial regression template

--- machine_learning/p_reg.py
+++ machine_learning/p_reg.py
@@ -17,35 +17,45 @@ from sklearn.linear_model import LinearRegression
 # Veri Yükleme
 veriler = pd.read_csv('maaslar.csv')
 
+# DataFrame (Slice)
 x = veriler.iloc[:,1:2]
 y = veriler.iloc[:,2:]
+
+# NumPy array dönüşümü
 X = x.values
 Y = y.values
 
+# Linear Regression
 lin_reg = LinearRegression()
 lin_reg.fit(X,Y)
 
-plt.scatter(X,Y, color = 'blue')
-plt.plot(x, lin_reg.predict(X), color = 'red')
-plt.show()
 
-#Polynomial Regression
+
+# Polynomial Regression
 poly_reg = PolynomialFeatures(degree=2)
 x_poly = poly_reg.fit_transform(X)
 
 lin_reg2 = LinearRegression()
 lin_reg2.fit(x_poly,y)
+
+# 4. dereceden
+poly_reg3 = PolynomialFeatures(degree=4)
+x_poly3 = poly_reg3.fit_transform(X)
+
+lin_reg3 = LinearRegression()
+lin_reg3.fit(x_poly3,y)
+
+# Görselleştirme
+plt.scatter(X,Y, color = 'blue')
+plt.plot(x, lin_reg.predict(X), color = 'red')
+plt.show()
+
 plt.scatter(X,Y, color = 'blue')
 plt.plot(X, lin_reg2.predict(poly_reg.fit_transform(X)), color = 'red')
 plt.show()
 
-poly_reg = PolynomialFeatures(degree=4)
-x_poly = poly_reg.fit_transform(X)
-
-lin_reg2 = LinearRegression()
-lin_reg2.fit(x_poly,y)
 plt.scatter(X,Y, color = 'blue')
-plt.plot(X, lin_reg2.predict(poly_reg.fit_transform(X)), color = 'red')
+plt.plot(X, lin_reg3.predict(poly_reg3.fit_transform(X)), color = 'red')
 plt.show()
 
 
