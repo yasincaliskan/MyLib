837a5f0f94a57964ca4b95aced6435aa057cce1c
Yasin Çalışkan <yaskocaliskan26@gmail.com>
2019-10-04 19:15:01 +0300

    support vector machine

---
+++ machine_learning/svc.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 19 22:20:50 2019
+
+@author: Yasko
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, Imputer, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+
+
+#1) Veri Önişleme
+#1.1) Veri Yükleme
+veriler = pd.read_csv('veriler.csv')
+
+x = veriler.iloc[:,1:4].values
+y = veriler.iloc[:,4:].values
+
+# Verilerin train-test olarak bölünmesi
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.33, random_state = 0)
+
+# Verilerin ölçeklenmesi - Standartlaştırma
+sc = StandardScaler()
+X_train = sc.fit_transform(x_train)
+X_test = sc.transform(x_test)
+
+# Logistic Regression
+logr = LogisticRegression(random_state=0)
+logr.fit(X_train, y_train)
+
+y_pred = logr.predict(X_test)
+print(y_pred)
+print(y_test)
+
+# Confusion Matrix (Karmaşıklık Matrisi)
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+# Tahminin doğruluğunu gösteren matrisi verir.
+svc = SVC(kernel = 'rbf')
+svc.fit(X_train, y_train)
+
+y_pred = svc.predict(X_test)
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
