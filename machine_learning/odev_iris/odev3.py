2f5884337477f125515baea53680ef6af7df8477
Yasin Çalışkan <yaskocaliskan26@gmail.com>
2019-10-06 18:22:32 +0300

    iris

---
+++ machine_learning/odev_iris/odev3.py
@@ -0,0 +1,122 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 19 22:20:50 2019
+
+@author: Yasko
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, Imputer, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+
+
+#1) Veri Önişleme
+#1.1) Veri Yükleme
+veriler = pd.read_excel('iris.xls')
+
+x = veriler.iloc[:,1:4].values
+y = veriler.iloc[:,4:].values
+
+# Verilerin train-test olarak bölünmesi
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.33, random_state = 0)
+
+# Verilerin ölçeklenmesi - Standartlaştırma
+sc = StandardScaler()
+X_train = sc.fit_transform(x_train)
+X_test = sc.transform(x_test)
+
+# Logistic Regression
+logr = LogisticRegression(random_state=0)
+logr.fit(X_train, y_train)
+
+y_pred = logr.predict(X_test)
+
+# Confusion Matrix (Karmaşıklık Matrisi) - Tahminin doğruluğunu gösteren matrisi verir.
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+# KNN
+knn = KNeighborsClassifier(n_neighbors=1, metric='minkowski')
+knn.fit(X_train, y_train)
+
+y_pred = knn.predict(X_test)
+cm = confusion_matrix(y_test, y_pred)
+print('KNN')
+print(cm)
+
+# Tahminin doğruluğunu gösteren matrisi verir.
+svc = SVC(kernel = 'rbf')
+svc.fit(X_train, y_train)
+
+y_pred = svc.predict(X_test)
+cm = confusion_matrix(y_test, y_pred)
+print('SVC')
+print(cm)
+
+# Naive Bayes
+gnb = GaussianNB()
+gnb.fit(X_train, y_train)
+
+y_pred = gnb.predict(X_test)
+
+cm = confusion_matrix(y_test, y_pred)
+print('Naive Bayes')
+print(cm)
+
+# Decision Tree Classifier
+dtc = DecisionTreeClassifier(criterion='entropy')
+dtc.fit(X_train, y_train)
+y_pred = dtc.predict(X_test)
+
+cm = confusion_matrix(y_test, y_pred)
+print('Decision Tree Cassifier')
+print(cm)
+
+# Random Forest Classifier
+rfc = RandomForestClassifier(n_estimators = 10, criterion = 'entropy')
+rfc.fit(X_train, y_train)
+
+y_pred = rfc.predict(X_test)
+cm = confusion_matrix(y_test, y_pred)
+print('Random Forest Classifier')
+print(cm)
+
+# Roc, TPR, FPR
+y_proba = rfc.predict_proba(X_test)
+fpr, tpr, thold = metrics.roc_curve(y_test, y_proba[:,0], pos_label='e')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
