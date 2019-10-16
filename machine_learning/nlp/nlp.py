c3315f7a3709f8b4121a87429ace7453e2b6f330
Yasin Çalışkan <yaskocaliskan26@gmail.com>
2019-10-16 14:01:00 +0300

    Update nlp.py

--- machine_learning/nlp/nlp.py
+++ machine_learning/nlp/nlp.py
@@ -10,17 +10,41 @@ import pandas as pd
 import re
 import nltk
 from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
 
 yorumlar = pd.read_csv('Restaurant_Reviews.csv')
 
-# Noktalama işaretlerini çıkarır.
-yorum = re.sub('[^a-zA-Z]',' ',yorumlar['Review'][0])
+nltk.download('stopwords') # Stopwords indirildi.
 
-# Küçük harfe çevirip listeye atar.
-yorum = yorum.lower().split()
+ps = PorterStemmer() # Kelime gövdeleri / Stemmer
 
-# Stopwords indirildi.
-nltk.download('stopwords')
+# Preprocessing
+derlem = []
+for i in range(1000):
+    yorum = re.sub('[^a-zA-Z]',' ',yorumlar['Review'][i]) # Noktalama işaretlerini çıkarır.
+    yorum = yorum.lower().split() # Küçük harfe çevirip listeye atar.
+    yorum = [ps.stem(kelime) for kelime in yorum if not kelime in set(stopwords.words('english'))]
+    yorum = ' '.join(yorum)
+    derlem.append(yorum)
 
-# Kelime gövdeleri / Stemmer
-ps = PorterStemmer()
+# Feature Extraction (Öznitelik çıkarımı) - Bag of Words
+cv = CountVectorizer(max_features = 1000)
+X = cv.fit_transform(derlem).toarray() # bağımsız değişken
+y = yorumlar.iloc[:,1].values # bağımlı değişken
+
+# Eğitim
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Gaussian Bayes Classification
+gnb = GaussianNB()
+gnb.fit(X_train, y_train)
+
+y_pred = gnb.predict(X_test)
+
+# Confusion Matrix ile doğruluk
+cm = confusion_matrix(y_test, y_pred)
+print(cm) # %72.5 accuracy
