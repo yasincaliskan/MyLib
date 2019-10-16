0bff5a45b751b77be9812ac3873d6b0b08c5fbc5
Yasin Çalışkan <yaskocaliskan26@gmail.com>
2019-10-16 13:15:56 +0300

    Update nlp.py

--- machine_learning/nlp/nlp.py
+++ machine_learning/nlp/nlp.py
@@ -8,7 +8,19 @@ Created on Wed Oct 16 12:40:51 2019
 import numpy as np
 import pandas as pd
 import re
+import nltk
+from nltk.stem.porter import PorterStemmer
 
 yorumlar = pd.read_csv('Restaurant_Reviews.csv')
 
+# Noktalama işaretlerini çıkarır.
 yorum = re.sub('[^a-zA-Z]',' ',yorumlar['Review'][0])
+
+# Küçük harfe çevirip listeye atar.
+yorum = yorum.lower().split()
+
+# Stopwords indirildi.
+nltk.download('stopwords')
+
+# Kelime gövdeleri / Stemmer
+ps = PorterStemmer()
